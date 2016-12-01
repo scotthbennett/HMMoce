f691a5bc2ff3b1ff2c4478e4ecd9a3ea970fb0a7
camrinbraun <camrin.braun@gmail.com>
2016-12-01 14:14:24 -0500

    examples are throwing errors in almost every case. need to figure out why but get rid of them for now

--- R/get.env.r
+++ R/get.env.r
@@ -14,9 +14,6 @@
 #'   
 #' @return nothing, just downloads the data to your local machine
 #' @export
-#' @examples
-#' splim <- list(xmin=-60, xmax=-40, ymin=10, ymax=30)
-#' get.env('2015-01-01', type = 'sst', splim, getwd())
 
 get.env <- function(uniqueDates, ptt, type = NA, spatLim, save.dir = getwd()){
   

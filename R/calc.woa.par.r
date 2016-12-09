607f9aeaaa7c371391357902b130d798bd30e497
camrinbraun <camrin.braun@gmail.com>
2016-12-09 11:03:35 -0500

    trying to fix calc woa par

--- R/calc.woa.par.r
+++ R/calc.woa.par.r
@@ -33,20 +33,22 @@
 #' # make dateVec, lat and lon
 #' 
 #' # define where woa is 
+#' get.env(type = 'woa', resol = 'one')
 #' 
 #' # GENERATE DAILY PROFILE LIKELIHOODS
-#' L.prof.woa <- calc.profile.par(pdt, dat = woa, lat = lat, lon = lon,
+#' L.prof.woa <- calc.woa.par(pdt, dat = woa, lat = lat, lon = lon,
 #'                            dateVec = dateVec, envType = 'woa')
 #' }
 #'
 
 calc.woa.par <- function(pdt, ptt, dat = NULL, lat = NULL, lon = NULL, dateVec, ncores = parallel::detectCores()){
+#calc.woa.par <- function(pdt, ptt, dat = NULL, dateVec, ncores = detectCores()){
   
   options(warn=-1)
   start.t <- Sys.time()
   
-  if(is.null(dat) | is.null(lat) | is.null(lon)){
-      stop('Error: dat, lat, lon must all be specified')
+  if(is.null(dat)){
+      stop('Error: data must be specified')
     }
   depth <- c(0, seq(2.5, 97.5, by=5), seq(112.5, 487.5, by=25), seq(525, 1475, by=50))
 
@@ -59,117 +61,135 @@ calc.woa.par <- function(pdt, ptt, dat = NULL, lat = NULL, lon = NULL, dateVec, 
   
   pdt$MidTemp <- (pdt$MaxTemp + pdt$MinTemp) / 2
   
-  print(paste0('Generating WOA likelihood for ', udates[1], ' through ', udates[length(udates)]))
+  print(paste0('Generating WOA profile likelihood for ', udates[1], ' through ', udates[length(udates)]))
   print('processing in parallel... ')
   
   cl = parallel::makeCluster(ncores)
   doParallel::registerDoParallel(cl, cores = ncores)
   
-  L.prof <- array(0, dim = c(dim(dat)[1:2], length(dateVec)))
+  L.prof <- array(0, dim = c(length(dat$lon), length(dat$lat), length(dateVec)))
   
 ans = foreach(i = 1:T) %dopar%{
-    
-    # define time based on tag data
-    time <- as.Date(udates[i])
-    pdt.i <- pdt[which(pdt$Date == time),]
-    
-    #extracts depth from tag data for day i
-    y <- pdt.i$Depth[!is.na(pdt.i$Depth)] 
-    y[y < 0] <- 0
-    
-    #extract temperature from tag data for day i
-    x <- pdt.i$MidTemp[!is.na(pdt.i$Depth)]  
-    
-    # use the which.min
-    depIdx = apply(as.data.frame(pdt.i$Depth), 1, FUN=function(x) which.min((x-depth)^2))
-    
-    # make predictions based on the regression model earlier for the temperature at standard WOA depth levels for low and high temperature at that depth
-    fit.low <- locfit::locfit(pdt.i$MinTemp ~ pdt.i$Depth)
-    fit.high <- locfit::locfit(pdt.i$MaxTemp ~ pdt.i$Depth)
-    n = length(depth[depIdx])
-    
-    pred.low <- stats::predict(fit.low, newdata = depth[depIdx], se = T, get.data = T)
-    pred.high <- stats::predict(fit.high, newdata = depth[depIdx], se = T, get.data = T)
-    
-    # data frame for next step
-    df = data.frame(low = pred.low$fit - pred.low$se.fit * sqrt(n),
-                    high = pred.high$fit + pred.high$se.fit * sqrt(n),
-                    depth = depth[depIdx])
-    
-
-      
-      if(i == 1){
-        pdtMonth <- as.numeric(format(as.Date(pdt.i$Date), format = '%m'))[1]
-      } 
-      
-      newMonth <- as.numeric(format(as.Date(pdt.i$Date), format = '%m'))[1]
-      
-      if(i == 1 | newMonth != pdtMonth) {
-        # calculates sd but "if" statement ensures it is only calculated at
-        # the beginning and when the date switches into a new month
-        # because it's relatively computationally intensive
-        
-        pdtMonth <- as.numeric(format(as.Date(pdt.i$Date), format = '%m'))[1]
-        dat.i = dat[,,,pdtMonth] #extract months climatology
-        
-        # calculate sd using Le Bris neighbor method and focal()
-        # sd.i = array(NA, dim = c(dim(dat.i)[1:2], length(depth)))
-        sd.i = array(NA, dim = c(dim(dat.i)[1:2], length(depIdx)))
-        
-        for(ii in 1:length(depIdx)){
-          r = raster::flip(raster::raster(t(dat.i[,,ii])), 2)
-          f1 = raster::focal(r, w = matrix(1, nrow = 3, ncol = 3), fun = function(x) stats::sd(x, na.rm = T))
-          f1 = t(raster::as.matrix(raster::flip(f1, 2)))
-          sd.i[,,ii] = f1
-        }
-        
-      }
-    
-    print(paste('Calculating likelihood for ', as.Date(time), '...', sep=''))
-
-    # setup the likelihood array for each day. Will have length (dim[3]) = n depths
-    lik.pdt = array(NA, dim = c(dim(dat.i)[1], dim(dat.i)[2], length(depIdx)))
-
-    for (b in 1:length(depIdx)) {
-        #calculate the likelihood for each depth level, b
-        lik.pdt[,,b] = likint3(dat.i[,,b], sd.i[,,b], df[b,1], df[b,2])
-      }
-    # multiply likelihood across depth levels for each day
-    lik.pdt <- apply(lik.pdt, 1:2, prod, na.rm = F)
+  
+  # define time based on tag data
+  time <- as.Date(udates[i])
+  pdt.i <- pdt[which(pdt$Date == time), ]
+  
+  #extracts depth from tag data for day i
+  y <- pdt.i$Depth[!is.na(pdt.i$Depth)]
+  y[y < 0] <- 0
+  
+  #extract temperature from tag data for day i
+  x <- pdt.i$MidTemp[!is.na(pdt.i$Depth)]
+  
+  # use the which.min
+  depIdx = apply(
+    as.data.frame(pdt.i$Depth),
+    1,
+    FUN = function(x)
+      which.min((x - depth) ^ 2)
+  )
+  
+  # make predictions based on the regression model earlier for the temperature at standard WOA depth levels for low and high temperature at that depth
+  fit.low <- locfit::locfit(pdt.i$MinTemp ~ pdt.i$Depth)
+  fit.high <- locfit::locfit(pdt.i$MaxTemp ~ pdt.i$Depth)
+  
+  n = length(depth[depIdx])
+  
+  pred.low <- stats::predict(fit.low, newdata = depth[depIdx], se = T, get.data = T)
+  pred.high <- stats::predict(fit.high, newdata = depth[depIdx], se = T, get.data = T)
+  
+  # data frame for next step
+  df = data.frame(
+    low = pred.low$fit - pred.low$se.fit * sqrt(n),
+    high = pred.high$fit + pred.high$se.fit * sqrt(n),
+    depth = depth[depIdx]
+  )
+  
+  pdtMonth <-
+    as.numeric(format(as.Date(pdt.i$Date), format = '%m'))[1]
+  
+  wdat = dat[[1]]
+  
+  dat.i = wdat[, , , pdtMonth] #extract months climatology
+  
+  # calculate sd using Le Bris neighbor method and focal()
+  # sd.i = array(NA, dim = c(dim(dat.i)[1:2], length(depth)))
+  
+  sd.i = array(NA, dim = c(dim(dat.i)[1:2], length(depIdx)))
+  
+  for (ii in 1:length(depIdx)) {
+    r = raster::flip(raster::raster(t(dat.i[, , ii])), 2)
+    f1 = raster::focal( r,
+      w = matrix(1, nrow = 3, ncol = 3),
+      fun = function(x)
+        stats::sd(x, na.rm = T)
+    )
+    f1 = t(raster::as.matrix(raster::flip(f1, 2)))
+    sd.i[, , ii] = f1
   }
   
-parallel::stopCluster(cl)
+#parallel::stopCluster(cl)
 
 # make index of dates for filling in lik.prof
 
 didx = base::match(udates, dateVec)
-
-# 
-# # identify date index and add completed likelihood to L.pdt array    
-# idx <- which(dateVec == as.Date(time))
-# L.prof[,,idx] = (lik.pdt / max(lik.pdt, na.rm=T)) - 0.2
-
-# lapply 
-lik.pdt = lapply(ans, function(x) x/max(x, na.rm = T)-0.2)
-
-ii = 1
-for(i in didx){
-  L.prof[,,i] = lik.pdt[[ii]]
-  ii = ii+1  
+  # }
+  
+  print(paste('Calculating likelihood for ', as.Date(time), '...', sep =
+                ''))
+  
+  # setup the likelihood array for each day. Will have length (dim[3]) = n depths
+  lik.pdt = array(NA, dim = c(dim(dat.i)[1], dim(dat.i)[2], length(depIdx)))
+  
+  for (b in 1:length(depIdx)) {
+    #calculate the likelihood for each depth level, b
+    lik.pdt[, , b] = likint3(dat.i[, , b], sd.i[, , b], df[b, 1], df[b, 2])
+  }
+  # multiply likelihood across depth levels for each day
+  lik.pdt <- apply(lik.pdt, 1:2, prod, na.rm = F)
 }
 
+parallel::stopCluster(cl)
+
+# make index of dates for filling in lik.prof
+didx = match(udates, dateVec)
+
+# lapply to normalize
+lik.pdt = lapply(ans, function(x)
+  (x / max(x, na.rm = T)))
+
+# Fill in the L.prof from the list output
+ii = 1
+for (i in didx) {
+  L.prof[, , i] = lik.pdt[[ii]]
+  ii = ii + 1
+}
 
 print(paste('Making final likelihood raster...'))
-  
+
 crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
-list.pdt <- list(x = lon, y = lat, z = L.prof)
-ex <- raster::extent(list.pdt)
-L.prof <- raster::brick(list.pdt$z, xmn = ex[1], xmx = ex[2], ymn = ex[3], ymx = ex[4], transpose = T, crs)
-L.prof <- raster::flip(L.prof, direction = 'y')
+L.ras <-
+  raster::brick(
+    L.prof,
+    xmn = min(dat$lon),
+    xmx = max(dat$lon),
+    ymn = min(dat$lat),
+    ymx = max(dat$lat),
+    transpose = T,
+    crs
+  )
+L.ras <- raster::flip(L.ras, direction = 'y')
+
+print(Sys.time() - start.t)
 
 options(warn = 2)
-L.prof
+L.ras
   
 }
 
+# load('C:/Users/benjamin.galuardi/Documents/GitHub/CAM_DATA/woa.one.rda')
+# load('C:/Users/benjamin.galuardi/Documents/GitHub/CAM_DATA/blue259_forParallel.RData')
+# 
+# res = calc.woa.par(pdt, dat = woa.one, dateVec = dateVec)
 

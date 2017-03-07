1e354c2120dbe17c7db3701dee5551c37e1148a7
camrinbraun <camrin.braun@gmail.com>
2017-03-06 21:43:23 -0500

    experimenting with a change to likint3 that will decrease the rapid dropoff we see at the edge of the likelihood. before this was so rapid because we trimmed the data layer to between min and max of the observations. may not work as well for non-sst integration but that's what this was tested on.

--- R/likint3.r
+++ R/likint3.r
@@ -11,7 +11,7 @@
 #' @export 
 
 likint3 <- function(w, wsd, minT, maxT){
-  widx = w >= minT & w <= maxT & !is.na(w)
+  widx = w >= minT-1 & w <= maxT+1 & !is.na(w)
   wdf = data.frame(w = as.vector(w[widx]), wsd = as.vector(wsd[widx]))
   wdf$wsd[is.na(wdf$wsd)] = 1e-3
   # wint = apply(wdf, 1, function(x) pracma::integral(dnorm, minT, maxT, mean = x[1], sd = x[2]))

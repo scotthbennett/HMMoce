7815052bec481d3fed345390d6ce9c9109308fff
camrinbraun <camrin.braun@gmail.com>
2017-03-14 00:45:35 +0000

    change make.L.mod to the new make.L function and get rid of the old

--- R/make.L.r
+++ R/make.L.r
@@ -13,6 +13,8 @@
 #' @param dateVec is vector of dates from tag to pop-up date by day. Only required if known.locs is not NULL.
 #' @param locs.grid is output grid from \code{setup.locs.grid}. Only required if known.locs is not NULL.
 #' @param iniloc is matrix of tag and pop locations. Default is NULL because this should be taken care of elsewhere.
+#' @param bathy is bathymetry raster (likely from ETOPO1) as acquired by \code{get.bath.data}
+#' @param pdt is data frame output from read.wc(type='pdt')
 
 #' @return a list containing: L, the overall likelihood array and L.mle, a more 
 #'   coarse version of L used later for parameter estimation
@@ -21,8 +23,19 @@
 #'   data sources. This will be expanded in the future based on user needs.
 #'   
 
-make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateVec = NULL, locs.grid = NULL, iniloc = NULL){
+make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateVec = NULL,
+                       locs.grid = NULL, iniloc = NULL, bathy=NULL, pdt=NULL){
 
+  if(class(L1) == 'list'){
+    if(length(L1) == 3){
+      L3 <- L1[[3]]; L2 <- L1[[2]]; L1 <- L1[[1]]
+    } else if (length(L1) == 2){
+      L2 <- L1[[2]]; L1 <- L1[[1]]
+    }
+  }
+    
+  if(!is.null(bathy) & is.null(pdt)) stop('Error: if bathy is not NULL then a maxDep vector must be supplied.')
+  
   if(!is.null(known.locs)){
     print('Input known locations are being used...')
     # convert input date, lat, lon to likelihood surfaces with dim(L1)
@@ -36,8 +49,8 @@ make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateV
     lon <- locs.grid$lon[1,]
     lat <- locs.grid$lat[,1]
     
-    idx <- which(dateVec %in% known.locs$date)
-    for(i in idx){
+    kn.idx <- which(dateVec %in% known.locs$date)
+    for(i in kn.idx){
       known.locs.i <- known.locs[which(known.locs$date %in% dateVec[i]),]
       
       if(length(known.locs.i[,1]) > 1){
@@ -49,7 +62,7 @@ make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateV
       y = which.min((known.locs.i$lat - lat) ^ 2)
 
       # assign the known location for this day, i, as 1 (known) in likelihood raster
-      L.locs[[i]][raster::cellFromXY(L.locs[[idx]], known.locs.i[,c(3,2)])] <- 1
+      L.locs[[i]][raster::cellFromXY(L.locs[[kn.idx]], known.locs.i[,c(3,2)])] <- 1
       
     }
     
@@ -193,9 +206,10 @@ make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateV
      }
   }
   
+  # normalize
   sumIdx <- which(raster::cellStats(L, sum, na.rm = T) != 0)
   for (i in sumIdx){
-    L[[i]] <- L[[i]] / raster::cellStats(L[[i]], max, na.rm = T)
+    L[[i]] <- L[[i]] / (raster::cellStats(L[[i]], max, na.rm = T) * 1.25)
   }
   
   if(!is.null(iniloc)){
@@ -204,9 +218,9 @@ make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateV
     
     if(!exists('L.locs')){
       L.locs <- L1 * 0
-      idx <- c(1, length(dateVec))
+      kn.idx <- c(1, length(dateVec))
     } else{
-      idx <- c(1, idx, length(dateVec))
+      kn.idx <- c(1, kn.idx, length(dateVec))
     }
     
     # need lat/lon vectors from locs.grid
@@ -231,12 +245,39 @@ make.L <- function(L1, L2 = NULL, L3 = NULL, known.locs = NULL, L.mle.res, dateV
     #print(raster::cellStats(L.locs[[108]], max))
     #print(idx)
     # add known to L
-    for(bb in idx){
+    for(bb in kn.idx){
       L[[bb]] <- L.locs[[bb]]
     }
   }
   
+  #========
+  # Bathymetry mask
+  #========
+  if(!is.null(bathy)){
+    print('Starting bathymetry mask...')
+    
+    maxDep <- data.frame(dplyr::summarise(dplyr::group_by(pdt, Date), max(Depth)))
+    maxDep$Date <- as.Date(maxDep$Date)
+    maxDep.df <- data.frame(Date = dateVec)
+    maxDep.df <- merge(maxDep.df, maxDep, by = 'Date', all.x=T)
+    maxDep.df[which(maxDep.df[,2] <= 0), 2] <- 1
+    maxDep.df[which(is.na(maxDep.df[,2])), 2] <- 1
+    #b.idx <- which(!is.na(maxDep.df[,2]))
+    bathy <- raster::resample(bathy, L)
+    naLidx = which(raster::cellStats(L, sum, na.rm=T) != 0)
+    
+    for (i in naLidx){
+      #print(paste('iteration ',i,' of ', length(b.idx)))
+      b.i <- bathy
+      b.i[b.i <= -maxDep.df[i,2]] <- 1
+      b.i[b.i != 1] <- NA
+      L[[i]] <- L[[i]] * b.i
+      #plot(L[[i]] * b.i); world(add=T)
+    }
+  }
+  
   # CREATE A MORE COARSE RASTER FOR PARAMETER ESTIMATION LATER
+  print('Starting raster resample...')
   L.mle <- raster::resample(L, L.mle.res)
   
   #----------------------------------------------------------------------------------#

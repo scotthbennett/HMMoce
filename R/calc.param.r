1621bc143cf79f945de098d96b4ddfd8f144b99b
camrinbraun <camrin.braun@gmail.com>
2017-03-14 01:16:45 +0000

    renaming new calc.param2 to replace the old calc.param

--- R/calc.param.r
+++ R/calc.param.r
@@ -2,28 +2,29 @@
 #' 
 #' \code{calc.param} calculates movement parameters used to generate kernels
 #' 
-#' Movement parameters for each of two behavioral modes are generated using an input grid
-#' and user-input speed (m s) for at least the migratory state.
+#' Movement parameters for each of two behavioral modes are generated using an
+#' input grid and user-input speed (m s) for at least the migratory state.
 #' 
-#' @param migr.spd is numeric indicating movement speed of animal (in m s) while in migratory behavior state.
-#' @param resid.spd is numeric indicating movement speed of animal (in m s) while in resident behavior state. If not specified, default is 10 percent of migratory speed. 
+#' @param migr.spd is numeric indicating movement speed of animal (in m s) while
+#'   in migratory behavior state.
+#' @param resid.frac is numeric indicating percent of migratory speed that
+#'   should be used for resident-like movements. Default is 10% of migratory
+#'   speed.
 #' @param g is grid from \code{setup.grid}
-#' 
+#'   
 #' @export
 #' @return a list of movement parameters
 
-calc.param <- function(migr.spd, resid.spd = NULL, g){
+calc.param2 <- function(migr.spd, resid.frac = 0.1, g){
   
   migr.spd <- migr.spd / 1000 * 3600 * 24 / 111
-  migr <- migr.spd / g$dla
+  dims <- migr.spd / g$dla
   
-  if (is.null(resid.spd)){
-    resid.spd <- migr.spd / 10
-  }
- 
-  resid.spd <- resid.spd / 1000 * 3600 * 24 / 111
-  resid <- resid.spd / g$dla
+  resid <- dims * resid.frac
   
-  return(list(migr = migr, sig1 = migr, resid = resid, sig2 = resid / 4))
+  #resid.spd <- resid.spd / 1000 * 3600 * 24 / 111
+  #resid <- resid.spd / g$dla
+  
+  return(list(migr = dims, sig1 = dims, resid = dims, sig2 = resid))
     
 }

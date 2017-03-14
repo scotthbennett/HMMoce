83905d598a84e8833dac2328550f380f1b3d818e
camrinbraun <camrin.braun@gmail.com>
2017-03-14 01:17:29 +0000

    adding documentation and making function return a P.final whether or not that is calculated with expmax. add input variable p.guess

--- R/makePar.r
+++ R/makePar.r
@@ -1,4 +1,14 @@
-makePar <- function(migr.spd, grid, L.arr, calcP=FALSE){
+#' \code{makePar} gets parameters for subsequent filter/smoother
+#' 
+#' Function builds movement kernels for 2 different behavior states and calculates, if desired, switching probability using an expectation maximization routine
+#' 
+#' @param migr.spd is numeric input to \code{calc.param}
+#' @param grid is a grid output by \code{resample.grid} that corresponds to the extent and resolution of L.arr (below).
+#' @param L.arr is the likelihood array used for state switch probability calculation (see \code{expmax}). This is typically the L.mle array returned from \code{make.L} because it's typically more coarse (and thus faster) than the higher-resolution L array.
+#' @param p.guess is vector of length 2 indicating probability of staying in states 1 and 2, respectively
+#' @param calcP is logical indicating whether to use \code{expmax} to calculate state-switching probabilities
+
+makePar <- function(migr.spd, grid, L.arr, p.guess = c(0.7, 0.8), calcP=FALSE){
   # PROVIDE FIXED KERNEL PARAMETERS
   par0 <- calc.param2(migr.spd = migr.spd, g = grid)
   D1 <- unlist(par0[1:2]) # parameters for kernel 1. this is migratory behavior mode
@@ -8,16 +18,14 @@ makePar <- function(migr.spd, grid, L.arr, calcP=FALSE){
   K1 <- gausskern(D1[1], D1[2], muadv = 0)
   K2 <- gausskern(D2[1], D2[2], muadv = 0)
   
+  # RUN EXPECTATION-MAXIMIZATION ROUTINE FOR MATRIX, P (STATE SWITCH PROBABILITY)
+  P.init <- matrix(c(p.guess[1], 1 - p.guess[1], 1 - p.guess[2], p.guess[2]), 2, 2, byrow = TRUE)
+  
   if(calcP){
-    # MAKE A GUESS AT STATE SWITCHING PROBABILITY
-    p <- c(0.7, 0.8)
-    
-    # RUN EXPECTATION-MAXIMIZATION ROUTINE FOR MATRIX, P (STATE SWITCH PROBABILITY)
-    P.init <- matrix(c(p[1], 1 - p[1], 1 - p[2], p[2]), 2, 2, byrow = TRUE)
     P.final <- expmax(P.init, g = grid, L = L.arr, K1, K2, save = T)
     save.p <- P.final[[2]]; P.final <- P.final[[1]]
   } else{
-    P.final = NA
+    P.final <- P.init
   }
   
   return(list(K1 = K1, K2 = K2, P.final = P.final))

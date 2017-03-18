7347e9d67ff28f4c6ba4d6001d493bcfee332ba2
camrinbraun <camrin.braun@gmail.com>
2017-03-18 22:38:00 +0000

    hycom version of likint3 with larger bounds

---
+++ R/likint3.hycom.r
@@ -0,0 +1,22 @@
+#' Calculate density function and integrate between limits
+#' 
+#' #' \code{likint3} calculates density function for a normal distribution and integrates between limits
+#'  
+#' @param w is an array of grid values like sea surface temperature
+#' @param wsd is an array containing the sd of the grid values, usually from \code{raster::focal}
+#' @param minT is an integer representing the lower limit of the tag-measured variable (e.g. SST)
+#' @param maxT is an integer representing the upper limit of the tag-measured variable
+#' 
+#' @return an array of dim(w) that represents the likelihood of the tag-measured variable as compared to the input grid
+#' @export 
+
+likint3.hycom <- function(w, wsd, minT, maxT){
+  widx = w >= minT-5 & w <= maxT+5 & !is.na(w)
+  wdf = data.frame(w = as.vector(w[widx]), wsd = as.vector(wsd[widx]))
+  wdf$wsd[is.na(wdf$wsd)] = 1e-3
+  # wint = apply(wdf, 1, function(x) pracma::integral(dnorm, minT, maxT, mean = x[1], sd = x[2]))
+  wint = apply(wdf, 1, function(x) stats::integrate(stats::dnorm, minT, maxT, mean = x[1], sd = x[2] * 2)$value) 
+  w = w * 0
+  w[widx] = wint
+  w
+} 

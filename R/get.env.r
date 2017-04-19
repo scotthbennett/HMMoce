98f3cc350549383ec3ba703adbbf8e95dc880de0
camrinbraun <camrin.braun@gmail.com>
2017-04-19 01:09:08 +0000

    sst type argument

--- R/get.env.r
+++ R/get.env.r
@@ -16,24 +16,42 @@
 #' @return nothing, just downloads the data to your local machine
 #' @export
 
-get.env <- function(uniqueDates = NULL, ptt = NULL, type = NULL, spatLim = NULL, resol = NULL, save.dir = getwd()){
+get.env <- function(uniqueDates = NULL, ptt = NULL, type = NULL, spatLim = NULL, resol = NULL, save.dir = getwd(), sst.type=NULL){
   
   if(is.null(type)){
     
     stop('Type of environmental data desired not specified.')
     
   } else if(type == 'sst'){
+    if(is.null(sst.type)){
+      warning('Warning: if type=sst then sst.type should be specified. Using default GHRsst.')
+      sst.type = 'ghr'
+    }
     
-    for(i in 1:length(uniqueDates)){
-      time <- as.Date(uniqueDates[i])
-      repeat{
-        get.oi.sst(spatLim, time, filename = paste(ptt, '_', time, '.nc', sep = ''), download.file = TRUE, dir = save.dir) # filenames based on dates from above
-        tryCatch({
-          err <- try(RNetCDF::open.nc(paste(save.dir, ptt, '_', time, '.nc', sep = '')), silent = T)
-        }, error=function(e){print(paste('ERROR: Download of data at ', time, ' failed. Trying call to server again.', sep = ''))})
-        if(class(err) != 'try-error') break
+    if(sst.type == 'oi'){
+      for(i in 1:length(uniqueDates)){
+        time <- as.Date(uniqueDates[i])
+        repeat{
+          get.oi.sst(spatLim, time, filename = paste(ptt, '_', time, '.nc', sep = ''), download.file = TRUE, dir = save.dir) # filenames based on dates from above
+          tryCatch({
+            err <- try(RNetCDF::open.nc(paste(save.dir, ptt, '_', time, '.nc', sep = '')), silent = T)
+          }, error=function(e){print(paste('ERROR: Download of data at ', time, ' failed. Trying call to server again.', sep = ''))})
+          if(class(err) != 'try-error') break
+        }
+      }
+    } else if(sst.type == 'ghr'){
+      for(i in 1:length(uniqueDates)){
+        time <- as.Date(uniqueDates[i])
+        repeat{
+          get.ghr.sst(spatLim, time, filename = paste(ptt, '_', time, '.nc', sep = ''), download.file = TRUE, dir = save.dir) # filenames based on dates from above
+          tryCatch({
+            err <- try(RNetCDF::open.nc(paste(save.dir, ptt, '_', time, '.nc', sep = '')), silent = T)
+          }, error=function(e){print(paste('ERROR: Download of data at ', time, ' failed. Trying call to server again.', sep = ''))})
+          if(class(err) != 'try-error') break
+        }
       }
     }
+    
 
   } else if(type == 'hycom'){
     

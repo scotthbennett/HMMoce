dac72e30ab62044605c7329d205b02e0cf22c39c
camrinbraun <camrin.braun@gmail.com>
2020-07-19 09:36:28 -0700

    new bottom temperature likelihood fun

--- R/calc.bottomTemp.par.r
+++ R/calc.bottomTemp.par.r
@@ -1,148 +1,180 @@
+#' Calculate bt-based likelihood in parallel
+#' 
+#' \code{calc.bottomTemp.par} compares tag-measured bottom temperature to a bottom temperature grid and calculates 
+#' likelihoods in parallel
+#' 
+#' @param bt is data frame containing tag-collected bottom temperature data. Requires at least cols: Date (POSIXct), Temperature
+#' @param filename is the first part of the filename specified to the download 
+#'   function \code{\link{get.env}}. For example, if downloaded files were 
+#'   specific to a particular dataset, you may want to identify that with a name
+#'   like 'tuna' or 'shark1'. This results in a downloaded filename of, for 
+#'   example, 'tuna_date.nc'. This filename is required here so the calc 
+#'   function knows where to get the env data.
+#' @param bt.dir local directory where bottom temp nc grids are stored
+#' @param dateVec is vector of POSIXct dates for each time step of the likelihood
+#' @param focalDim is integer for dimensions of raster::focal used to calculate 
+#'   sd() of env grid cell. If left to NULL (default), this dimension will
+#'   approximately incorporate 0.25 degrees.
+#' @param sens.err is numeric indicating the percent sensor error in the tag bottom temp
+#'   sensor. Default is 1.
+#' @param varName is name of temperature variable in the bottom temperature nc grids. Default is "Temperature".
+#' @param ncores is integer indicating number of cores used in this parallel
+#'  computation. Defaults to using a detection function that chooses cores for
+#'  you.
+#'   
+#' @return likelihood is raster brick of likelihood surfaces representing
+#'   matches between tag-based bottom temperature and environmental bottom temperature grids
+#'   
+#' @export
+#' 
+#' @seealso \code{\link{calc.sst.par}}
+#'   
 
-calc.bottomTemp.par=function (tag.pdt,bathy.grid, dateVec, focalDim = NULL, sens.err = 1,filename='hycom',hycom.dir,ncores=4){
-  # hycom.dir='C:/Users/pgatti/Documents/Halibut/R/functions/whoi_hmmoce_pkg/EnvData/hycom/'
-  # filename='hycom'
-  # required functions & packages
-  require(parallel)
-  require(doParallel)
-  require(foreach)
-  # functions needed in parallel
-  ac=function(x){return(as.character(x))}
-  an=function(x){return(as.numeric(ac(x)))}
-  an.=function(x){return(as.numeric(x))}
-  likint3=function (w, wsd, minT, maxT){
-    widx <- !is.na(w)
-    wdf = data.frame(w = as.vector(w[widx]), wsd = as.vector(wsd[widx]))
-    wdf$wsd[is.na(wdf$wsd)] = 0.001
-    wint = apply(wdf, 1, function(x) stats::integrate(stats::dnorm, 
-                                                      # !!!!!! *2 ????
-                                                      # minT, maxT, mean = x[1], sd = x[2]*2 )$value)
-                                                      minT, maxT, mean = x[1], sd = x[2] )$value)
-    w = w * 0
-    w[widx] = wint
-    w
-  }
+calc.bottomTemp <- function(tag.bt, filename, bt.dir, dateVec, focalDim = NULL, sens.err = 1, varName = 'Temperature', ncores = NULL){
   
-  #dateVec=as.Date(substring(ac(tag.pdt$Date,1,10)))
-  #--------------------------
-  # hycom format
-  #--------------------------
-  # extract var names in ncdf env (hycom) files
-  nc1 = RNetCDF::open.nc(dir(hycom.dir, full.names = T)[1])
+  print(paste('Starting bottom temperature likelihood calculation...'))
+  
+  if (is.null(ncores)) ncores <- ceiling(parallel::detectCores() * .9)
+  if (is.na(ncores) | ncores < 0) ncores <- ceiling(as.numeric(system('nproc', intern=T)) * .9)
+  
+  t0 <- Sys.time()
+  
+  if(class(tag.bt$Date)[1] != 'POSIXct') stop('Error: tag.bt$Date must be as.POSIXct format.')
+  #if(class(dateVec)[1] != 'POSIXct') dateVec <- as.POSIXct(dateVec)
+  
+  tag.bt$dateVec <- findInterval(tag.bt$Date, dateVec)
+  by_dte <- dplyr::group_by(tag.bt, as.factor(tag.bt$dateVec))  # group by unique time points
+  tag.bt <- data.frame(dplyr::summarise_(by_dte, "min(Temperature)", "max(Temperature)"))
+  colnames(tag.bt) <- list('time', 'minT', 'maxT')
+  tag.bt$time <- dateVec[as.numeric(as.character(tag.bt$time))]
+  
+  T <- length(tag.bt[,1])
+  
+  print(paste('Starting iterations through deployment period ', '...'))
+  
+  # GET EVERYTHING SETUP BEFORE PARALLEL
+  time1 <- tag.bt$time[1]
+  bt1 <- c(tag.bt$minT[1] * (1 - sens.err), tag.bt$maxT[1] * (1 + sens.err)) # sensor error
+  
+  # open day's bt data
+  nc1 <- RNetCDF::open.nc(paste(bt.dir, filename, '_', as.Date(time1), '.nc', sep='')) #add lat lon in filename '.nc', sep=''))
+  
+  # get correct name in bt data
   ncnames = NULL
   nmax <- RNetCDF::file.inq.nc(nc1)$nvars - 1
-  for (ii in 0:nmax) ncnames[ii + 1] <- RNetCDF::var.inq.nc(nc1,ii)$name
+  for(ii in 0:nmax) ncnames[ii + 1] <- RNetCDF::var.inq.nc(nc1, ii)$name
+  nameidx <- grep(varName, ncnames, ignore.case = TRUE) - 1
+  dat <- RNetCDF::var.get.nc(nc1, nameidx)
+  lon <- RNetCDF::var.get.nc(nc1, 'longitude')
+  lat <- RNetCDF::var.get.nc(nc1, 'latitude')
   
-  # vars index
-  temp.idx <- grep("temp", ncnames, ignore.case = TRUE) - 1
-  lat.idx <- grep("lat", ncnames, ignore.case = TRUE) - 1
-  lon.idx <- grep("lon", ncnames, ignore.case = TRUE) - 1
-  dep.idx <- grep("dep", ncnames, ignore.case = TRUE) - 1
+  # calc sd of bt
+  # focal calc on mean temp and write to sd var
+  r = raster::flip(raster::raster(t(dat), xmn=min(lon), xmx=max(lon),
+                                  ymn=min(lat), ymx=max(lat)), 2)
   
-  # scale and offset extraction for temperature
-  ncatts <- NULL
-  nmax <- RNetCDF::var.inq.nc(nc1, temp.idx)$natts - 1
-  for (ii in 0:nmax) ncatts[ii + 1] <- RNetCDF::att.inq.nc(nc1,temp.idx, ii)$name
-  scale.idx <- grep("scale", ncatts, ignore.case = TRUE) -  1
-  if (length(scale.idx) != 0) {
-    scale <- RNetCDF::att.get.nc(nc1, temp.idx, attribute = scale.idx)
-  }else{
-    scale <- 1
-  }
-  off.idx <- grep("off", ncatts, ignore.case = TRUE) - 1
-  if (length(off.idx) != 0) {
-    offset <- RNetCDF::att.get.nc(nc1, temp.idx, attribute = off.idx)
-  }else{
-    offset <- 1
+  # check for coarse enough resolution that our calculations wont take all day
+  if(round(raster::res(r)[1], 2) < 0.1){
+    aggFact <- round(0.1 / round(raster::res(r)[1], 2), 0)
+    if(aggFact > 1) r <- raster::aggregate(r, fact = aggFact)
+  } 
+  
+  # set up a focal() dimension if not specified by user, we use this for sd() of surround 0.25deg worth of grid cells
+  if(is.null(focalDim)){
+    focalDim <- round(0.25 / raster::res(r)[1], 0)
+    if(focalDim %% 2 == 0) focalDim <- focalDim - 1
   }
   
-  # cell coordinates Depth, lat, lon
-  depth <- RNetCDF::var.get.nc(nc1, dep.idx)
-  lon <- RNetCDF::var.get.nc(nc1, lon.idx)
-  if (length(dim(lon)) == 2)  lon <- lon[, 1]
-  if (!any(lon < 180)) lon <- lon - 360
-  lat <- RNetCDF::var.get.nc(nc1, lat.idx)
-  if (length(dim(lat)) == 2) lat <- lat[1, ]
+  # calculate sd from bt grid using focal()
+  sdx = raster::focal(r, w = matrix(1, nrow = focalDim, ncol = focalDim), fun = function(x) stats::sd(x, na.rm = T))
+  sdx = t(raster::as.matrix(raster::flip(sdx, 2)))
   
-  # max layer
-  i=1
-  nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", dateVec[i], ".nc", sep = "")),T)
-  while(!class(nc)=="NetCDF"){
-    i=i+1
-    nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", dateVec[i], ".nc", sep = "")),T)
-  }
-  #rm(i)
-  dat <- RNetCDF::var.get.nc(nc, temp.idx) * scale + offset
-  matz=array(data=NA,dim=dim(dat))
-  for(z. in 1:dim(dat)[3]){
-    #z.=300
-    tt=dat[,,z.]
-    ind.=which(is.finite(tt) & !is.na(tt))
-    matz[,,z.][ind.]=z.
-  }
+  # re-calc dat after raster::aggregate, if it happened
+  dat <- base::t(raster::as.matrix(raster::flip(r, 2)))
   
-  mz=apply(matz,1:2,function(x)range(x,na.rm=T)[2])
-  mz[!is.finite(mz)]<-NA
+  # compare bt to that day's tag-based ohc
+  lik.bt <- likint3(dat, sdx, bt1[1], bt1[2])
   
-  # index of bottom cells
-  # need lon lat  z (indexes) *(nlon*nlat*(z-1))
-  ind.=1:length(mz)+dim(mz)[1]*dim(mz)[2]*as.vector(abs(mz)-1)
-  #--------------------------
-  # hycom format end
-  #--------------------------
+  # result will be array of likelihood surfaces
+  L.bt <- array(0, dim = c(dim(lik.bt), length(dateVec)))
   
-  print(paste("Starting bottom temp likelihood calculation..."))
-  t0 <- Sys.time()
-  tag.pdt$Date <- as.Date(as.POSIXct(tag.pdt$Date, format = findDateFormat(tag.pdt$Date)))
+  # get aggregated version of lat/lon for raster creation later
+  lon.agg <- seq(raster::extent(r)[1], raster::extent(r)[2], length.out=dim(r)[2])
+  lat.agg <- seq(raster::extent(r)[3], raster::extent(r)[4], length.out=dim(r)[1])
   
-  T <- length(tag.pdt[, 1])
-  print(paste("Starting iterations through deployment period ", "..."))
-  #L.btemp=array(data=NA,dim=c(dim(dat)[1:2],length(dateVec)))
-  # declare cluster parrallel
+  ## END of SETUP RUN
+  
+  print('Processing in parallel... ')
+  
   cl = parallel::makeCluster(ncores)
   doParallel::registerDoParallel(cl, cores = ncores)
-  ans=foreach::foreach(i = 1:T) %dopar% {
-    # i=1; sens.err=1
-    time <- tag.pdt$Date[i]
-    btemp.i <- c(tag.pdt$MinTemp[i] * (1 - sens.err/100), tag.pdt$MaxTemp[i] *(1 + sens.err/100))
+  
+  ans = foreach::foreach(i = 1:T) %dopar%{
     
-    nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", as.Date(time), ".nc", sep = "")),T)
-    # start if class netcdf
-    if(class(nc)=="NetCDF"){ 
-      # rescale temp data (matrix lon, lat, depth)
-      dat <- RNetCDF::var.get.nc(nc, temp.idx) * scale + offset
-      btemp=matrix(data=as.vector(dat)[ind.],ncol=dim(dat)[2],nrow=dim(dat)[1])
-      btemp.grid=raster::raster(btemp)
-      sdx = raster::focal(btemp.grid, w = matrix(1, nrow = focalDim, ncol = focalDim), fun = function(x) stats::sd(x,na.rm = T))
-      lik.btemp <- likint3(btemp.grid, sdx, btemp.i[1], btemp.i[2])
-      lik.btemp<-raster::as.matrix(lik.btemp)/max(raster::as.matrix(lik.btemp),na.rm=T)
-      lik.btemp[is.na(lik.btemp) | is.infinite(lik.btemp)]<-0
-    }else{
-      lik.btemp=matrix(data=0,nrow=dim(dat)[1],ncol=dim(dat)[2])
-    }
-    lik.btemp
+  #for(i in 1:T){
+    
+    time <- tag.bt$time[i]
+    bt.i <- c(tag.bt$minT[i] * (1 - sens.err / 100), tag.bt$maxT[i] * (1 + sens.err / 100)) # sensor error
+    
+    # open day's bt data
+    nc <- RNetCDF::open.nc(paste(bt.dir, filename, '_', as.Date(time), '.nc', sep='')) #add lat lon in filename '.nc', sep=''))
+    dat <- RNetCDF::var.get.nc(nc, nameidx) # for OI bt
+    
+    # calc sd of bt
+    # focal calc on mean temp and write to sd var
+    r <- raster::flip(raster::raster(t(dat), xmn=min(lon), xmx=max(lon),
+                                     ymn=min(lat), ymx=max(lat)), 2)
+    
+    if(round(raster::res(r)[1], 2) < 0.1){
+      aggFact <- round(0.1 / round(raster::res(r)[1], 2), 0)
+      if(aggFact != 1) r <- raster::aggregate(r, fact = aggFact)
+    } 
+    
+    sdx = raster::focal(r, w = matrix(1, nrow = focalDim, ncol = focalDim), fun = function(x) stats::sd(x, na.rm = T))
+    sdx = t(raster::as.matrix(raster::flip(sdx, 2)))
+    
+    dat <- base::t(raster::as.matrix(raster::flip(r, 2)))
+    
+    # compare bt to env grid
+    lik.bt <- likint3(dat, sdx, bt.i[1], bt.i[2])
+    
+    #idx <- which(dateVec == time)
+    #L.bt[,,idx] = lik.bt / max(lik.bt, na.rm=T)
+    
+    lik.bt
   }
+  
   parallel::stopCluster(cl)
   
-  L. <- array(0, dim = c(dim(ans[[1]])[1], dim(ans[[2]])[2], length(dateVec)))
-  # switch from list to array
-  for (i in 1:T) {
-    time <- tag.pdt$Date[i]
-    idx <- which(dateVec == as.Date(time))
-    L.[, , idx] = ans[[i]]
+  # make index of dates for filling in L.bt
+  
+  didx <- base::match(unique(tag.bt$time), dateVec)
+  
+  #lapply
+  lik.bt <- lapply(ans, function(x) x / max(x, na.rm = T))
+  
+  ii = 1
+  for (i in didx){
+    L.bt[,,i] <- lik.bt[[ii]]
+    ii <- ii + 1
   }
   
-  print(paste("Making final likelihood raster..."))
+  print(paste('Making final likelihood raster...'))
+  
   crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
-  #list.btemp <- list(x = lon.agg, y = lat.agg, z = L.btemp)
-  list.btemp <- list(x = lon, y = lat, z = L.)
-  ex <- raster::extent(list.btemp)
-  L.btemp <- raster::brick(list.btemp$z, xmn = ex[1], xmx = ex[2], 
-                           ymn = ex[3], ymx = ex[4], transpose = T, crs)
-  L.btemp <- raster::flip(L.btemp, direction = "y")
-  L.btemp[L.btemp < 0] <- 0
+  list.bt <- list(x = lon.agg, y = lat.agg, z = L.bt)
+  ex <- raster::extent(list.bt)
+  L.bt <- raster::brick(list.bt$z, xmn=ex[1], xmx=ex[2], ymn=ex[3], ymx=ex[4], transpose=T, crs)
+  L.bt <- raster::flip(L.bt, direction = 'y')
+  
+  L.bt[L.bt < 0] <- 0
+  
+  names(L.bt) <- as.character(dateVec)
+  
   t1 <- Sys.time()
-  print(paste("Bottom temp calculations took ", round(as.numeric(difftime(t1, 
-                                                                          t0, units = "mins")), 2), "minutes..."))
-  return(L.btemp)
+  print(paste('Bottom temperature calculations took ', round(as.numeric(difftime(t1, t0, units='mins')), 2), 'minutes...'))
+  
+  # return bt likelihood surfaces
+  return(L.bt)
+  
 }
--- R/calc.bottomTemp.r
+++ R/calc.bottomTemp.r
@@ -1,126 +1,120 @@
+#' Calculate bt-based likelihood
+#' 
+#' \code{calc.bottomTemp} compares tag-measured bottom temperature to a bottom temperature grid and calculates 
+#' likelihoods
+#' 
+#' @param bt is data frame containing tag-collected bottom temperature data. Requires at least cols: Date (POSIXct), Temperature
+#' @param filename is the first part of the filename specified to the download 
+#'   function \code{\link{get.env}}. For example, if downloaded files were 
+#'   specific to a particular dataset, you may want to identify that with a name
+#'   like 'tuna' or 'shark1'. This results in a downloaded filename of, for 
+#'   example, 'tuna_date.nc'. This filename is required here so the calc 
+#'   function knows where to get the env data.
+#' @param bt.dir local directory where bottom temp nc grids are stored
+#' @param dateVec is vector of POSIXct dates for each time step of the likelihood
+#' @param focalDim is integer for dimensions of raster::focal used to calculate 
+#'   sd() of env grid cell. If left to NULL (default), this dimension will
+#'   approximately incorporate 0.25 degrees.
+#' @param sens.err is numeric indicating the percent sensor error in the tag bottom temp
+#'   sensor. Default is 1.
+#' @param varName is name of temperature variable in the bottom temperature nc grids. Default is "Temperature".
+#'   
+#' @return likelihood is raster brick of likelihood surfaces representing
+#'   matches between tag-based bottom temperature and environmental bottom temperature grids
+#'   
+#' @export
+#' 
+#' @seealso \code{\link{calc.sst}}
+#'   
 
-calc.bottomTemp=function (tag.pdt,bathy.grid, dateVec, focalDim = NULL, sens.err = 1,filename='hycom',hycom.dir){
-  # hycom.dir='C:/Users/pgatti/Documents/Halibut/R/functions/whoi_hmmoce_pkg/EnvData/hycom/'
-  # filename='hycom'
-  # required functions & packages
-  # functions needed in parallel
-  ac=function(x){return(as.character(x))}
-  an=function(x){return(as.numeric(ac(x)))}
-  an.=function(x){return(as.numeric(x))}
+calc.bottomTemp <- function(tag.bt, filename, bt.dir, dateVec, focalDim = NULL, sens.err = 1, varName = 'Temperature'){
   
-  #--------------------------
-  # hycom format
-  #--------------------------
-  # extract var names in ncdf env (hycom) files
-  nc1 = RNetCDF::open.nc(dir(hycom.dir, full.names = T)[1])
-  ncnames = NULL
-  nmax <- RNetCDF::file.inq.nc(nc1)$nvars - 1
-  for (ii in 0:nmax) ncnames[ii + 1] <- RNetCDF::var.inq.nc(nc1,ii)$name
-  
-  # vars index
-  temp.idx <- grep("temp", ncnames, ignore.case = TRUE) - 1
-  lat.idx <- grep("lat", ncnames, ignore.case = TRUE) - 1
-  lon.idx <- grep("lon", ncnames, ignore.case = TRUE) - 1
-  dep.idx <- grep("dep", ncnames, ignore.case = TRUE) - 1
-  
-  # scale and offset extraction for temperature
-  ncatts <- NULL
-  nmax <- RNetCDF::var.inq.nc(nc1, temp.idx)$natts - 1
-  for (ii in 0:nmax) ncatts[ii + 1] <- RNetCDF::att.inq.nc(nc1,temp.idx, ii)$name
-  scale.idx <- grep("scale", ncatts, ignore.case = TRUE) -  1
-  if (length(scale.idx) != 0) {
-    scale <- RNetCDF::att.get.nc(nc1, temp.idx, attribute = scale.idx)
-  }else{
-    scale <- 1
-  }
-  off.idx <- grep("off", ncatts, ignore.case = TRUE) - 1
-  if (length(off.idx) != 0) {
-    offset <- RNetCDF::att.get.nc(nc1, temp.idx, attribute = off.idx)
-  }else{
-    offset <- 1
-  }
-  
-  # cell coordinates Depth, lat, lon
-  depth <- RNetCDF::var.get.nc(nc1, dep.idx)
-  lon <- RNetCDF::var.get.nc(nc1, lon.idx)
-  if (length(dim(lon)) == 2)  lon <- lon[, 1]
-  if (!any(lon < 180)) lon <- lon - 360
-  lat <- RNetCDF::var.get.nc(nc1, lat.idx)
-  if (length(dim(lat)) == 2) lat <- lat[1, ]
-  
-  # max layer
-  i=1
-  nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", dateVec[i], ".nc", sep = "")),T)
-  while(!class(nc)=="NetCDF"){
-    i=i+1
-    nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", dateVec[i], ".nc", sep = "")),T)
-  }
-  dat <- RNetCDF::var.get.nc(nc, temp.idx) * scale + offset
-  matz=array(data=NA,dim=dim(dat))
-  for(z. in 1:dim(dat)[3]){
-    #z.=300
-    tt=dat[,,z.]
-    ind.=which(is.finite(tt) & !is.na(tt))
-    matz[,,z.][ind.]=z.
-  }
-  
-  mz=apply(matz,1:2,function(x)range(x,na.rm=T)[2])
-  mz[!is.finite(mz)]<-NA
-  
-  # index of bottom cells
-  # need lon lat  z (indexes) *(nlon*nlat*(z-1))
-  ind.=1:length(mz)+dim(mz)[1]*dim(mz)[2]*as.vector(abs(mz)-1)
-  #--------------------------
-  # hycom format end
-  #--------------------------
-  
-  print(paste("Starting bottom temp likelihood calculation..."))
+  print(paste('Starting bottom temperature likelihood calculation...'))
   t0 <- Sys.time()
-  tag.pdt$Date <- as.Date(as.POSIXct(tag.pdt$Date, format = findDateFormat(tag.pdt$Date)))
   
-  T <- length(tag.pdt[, 1])
-  print(paste("Starting iterations through deployment period ", "..."))
-  L.btemp=array(data=0,dim=c(dim(dat)[1:2],length(dateVec)))
-  for (i in 1:T) {
-    # i=1; sens.err=1
-    time <- tag.pdt$Date[i]
-    btemp.i <- c(tag.pdt$MinTemp[i] * (1 - sens.err/100), tag.pdt$MaxTemp[i] *(1 + sens.err/100))
+  if(class(tag.bt$Date)[1] != 'POSIXct') stop('Error: tag.bt$Date must be as.POSIXct format.')
+  #if(class(dateVec)[1] != 'POSIXct') dateVec <- as.POSIXct(dateVec)
+  
+  tag.bt$dateVec <- findInterval(tag.bt$Date, dateVec)
+  by_dte <- dplyr::group_by(tag.bt, as.factor(tag.bt$dateVec))  # group by unique time points
+  tag.bt <- data.frame(dplyr::summarise_(by_dte, "min(Temperature)", "max(Temperature)"))
+  colnames(tag.bt) <- list('time', 'minT', 'maxT')
+  tag.bt$time <- dateVec[as.numeric(as.character(tag.bt$time))]
+  
+  T <- length(tag.bt[,1])
+  
+  print(paste('Starting iterations through deployment period ', '...'))
+  
+  for(i in 1:T){
     
-    nc <- try(RNetCDF::open.nc(paste(hycom.dir, filename, "_", as.Date(time), ".nc", sep = "")),T)
-    # start if class netcdf
-    if(class(nc)=="NetCDF"){ 
-      # rescale temp data (matrix lon, lat, depth)
-      dat <- RNetCDF::var.get.nc(nc, temp.idx) * scale + offset
-      btemp=matrix(data=as.vector(dat)[ind.],ncol=dim(dat)[2],nrow=dim(dat)[1])
-      btemp.grid=raster::raster(btemp)
-      sdx = raster::focal(btemp.grid, w = matrix(1, nrow = focalDim, ncol = focalDim), fun = function(x) stats::sd(x,na.rm = T))
-      lik.btemp <- likint3(btemp.grid, sdx, btemp.i[1], btemp.i[2])
-      idx <- which(dateVec == as.Date(time))
-      L.btemp[,,idx]<-raster::as.matrix(lik.btemp)/max(raster::as.matrix(lik.btemp),na.rm=T)
-      L.btemp[,,idx][is.na(L.btemp[,,idx]) | is.infinite(is.na(L.btemp[,,idx]))]<-0
-    }#else{
-    # 
-    #}
+    time <- tag.bt$time[i]
+    bt.i <- c(tag.bt$minT[i] * (1 - sens.err / 100), tag.bt$maxT[i] * (1 + sens.err / 100)) # sensor error
+    
+    # open day's bt data
+    nc <- RNetCDF::open.nc(paste(bt.dir, filename, '_', as.Date(time), '.nc', sep='')) #add lat lon in filename '.nc', sep=''))
+    
+    if (i == 1){
+      # get correct name in bt data
+      ncnames = NULL
+      nmax <- RNetCDF::file.inq.nc(nc)$nvars - 1
+      for(ii in 0:nmax) ncnames[ii + 1] <- RNetCDF::var.inq.nc(nc, ii)$name
+      nameidx <- grep(varName, ncnames, ignore.case=TRUE) - 1
+      
+      lon <- RNetCDF::var.get.nc(nc, 'longitude')
+      lat <- RNetCDF::var.get.nc(nc, 'latitude')
+    }
+    
+    dat <- RNetCDF::var.get.nc(nc, nameidx) # for OI bt
+    
+    # calc sd of bt
+    # focal calc on mean temp and write to sd var
+    r <- raster::flip(raster::raster(t(dat), xmn=min(lon), xmx=max(lon),
+                                     ymn=min(lat), ymx=max(lat)), 2)
+    
+    if(round(raster::res(r)[1], 2) < 0.1){
+      aggFact <- round(0.1 / round(raster::res(r)[1], 2), 0)
+      if(aggFact != 1) r <- raster::aggregate(r, fact = aggFact)
+    } 
+    
+    if(is.null(focalDim)){
+      focalDim <- round(0.25 / raster::res(r)[1], 0)
+      if(focalDim %% 2 == 0) focalDim <- focalDim - 1
+    }
+    
+    sdx = raster::focal(r, w = matrix(1, nrow = focalDim, ncol = focalDim), fun = function(x) stats::sd(x, na.rm = T))
+    sdx = t(raster::as.matrix(raster::flip(sdx, 2)))
+    dat <- base::t(raster::as.matrix(raster::flip(r, 2)))
+    
+    # compare bt to env grid
+    lik.bt <- likint3(dat, sdx, bt.i[1], bt.i[2])
+    
+    if(i == 1){
+      # result will be array of likelihood surfaces
+      L.bt <- array(0, dim = c(dim(lik.bt), length(dateVec)))
+      
+      lon.agg <- seq(raster::extent(r)[1], raster::extent(r)[2], length.out=dim(r)[2])
+      lat.agg <- seq(raster::extent(r)[3], raster::extent(r)[4], length.out=dim(r)[1])
+    }
+    
+    idx <- which(dateVec == time)
+    L.bt[,,idx] = lik.bt / max(lik.bt, na.rm=T)
+    
   }
-  #  lon.agg <- seq(raster::extent(btemp.grid)[1], raster::extent(btemp.grid)[2], 
-  #                 length.out = dim(btemp.grid)[2])
-  # lat.agg <- seq(raster::extent(btemp.grid)[3], raster::extent(btemp.grid)[4], 
-  #                 length.out = dim(btemp.grid)[1])
   
-  #L.btemp=aperm(L.btemp,c(2,1,3))
-  #L.btemp=L.btemp[,dim(L.btemp)[2]:1,]
+  print(paste('Making final likelihood raster...'))
   
-  print(paste("Making final likelihood raster..."))
   crs <- "+proj=longlat +datum=WGS84 +ellps=WGS84"
-  #list.btemp <- list(x = lon.agg, y = lat.agg, z = L.btemp)
-  list.btemp <- list(x = lon, y = lat, z = L.btemp)
-  ex <- raster::extent(list.btemp)
-  L.btemp <- raster::brick(list.btemp$z, xmn = ex[1], xmx = ex[2], 
-                           ymn = ex[3], ymx = ex[4], transpose = T, crs)
-  L.btemp <- raster::flip(L.btemp, direction = "y")
-  L.btemp[L.btemp < 0] <- 0
+  list.bt <- list(x = lon.agg, y = lat.agg, z = L.bt)
+  ex <- raster::extent(list.bt)
+  L.bt <- raster::brick(list.bt$z, xmn=ex[1], xmx=ex[2], ymn=ex[3], ymx=ex[4], transpose=T, crs)
+  L.bt <- raster::flip(L.bt, direction = 'y')
+  
+  L.bt[L.bt < 0] <- 0
+  
   t1 <- Sys.time()
-  print(paste("Bottom temp calculations took ", round(as.numeric(difftime(t1, 
-                                                                          t0, units = "mins")), 2), "minutes..."))
-  return(L.btemp)
+  print(paste('Bottom temperature calculations took ', round(as.numeric(difftime(t1, t0, units='mins')), 2), 'minutes...'))
+  
+  # return bt likelihood surfaces
+  return(L.bt)
+  
 }

8d84fe078025dae12abd685d811b6932a593ce3c
camrinbraun <camrin.braun@gmail.com>
2020-07-21 10:16:16 -0700

    clean and document log likelihood funs

--- R/neg.log.lik.fun.r
+++ R/neg.log.lik.fun.r
@@ -1,40 +1,46 @@
+#' Calculate negative log likelihood
+#' 
+#' \code{neg.log.lik.fun} calculates the negative log likelihood
+#' 
+#' @param pars kernel parameters. Usually output from \code{est_params}
+#' @param g see \code{hmm.filter}
+#' @param L see \code{hmm.filter}
+#' @param maskL see \code{hmm.filter}
+#' @param bound.thr see \code{hmm.filter}
+#' @param minBounds see \code{hmm.filter}
+#' @param kernel character either "normal" or "isotropic", depending on desired kernel characteristics
+#' 
+#' @return a negative log likelihood
+#' @author Paul Gatti
+#' 
 
-neg.log.lik.fun <- function(pars,g,L,maskL=TRUE,bound.thr = 0.1, minBounds = 10){
-  # pars=c(3,1,.7,.8)
-  sigmas=pars[1:2]
-  sizes=rep(ceiling(sigmas[1]*4),2)
-  pb=pars[3:4]
-  muadvs=c(0,0)
+neg.log.lik.fun <- function(pars, g, L, maskL = TRUE, bound.thr = 0.1, minBounds = 10, kernel = 'normal'){
   
-  # if(!is.na(pars[5])) muadvs[1]<-pars[5]
-  #  if(!is.na(pars[6])) muadvs[2]<-pars[6]
+  sigmas = pars[1:2]
+  sizes = rep(ceiling(sigmas[1]*4),2)
+  pb = pars[3:4]
+  muadvs = c(0,0)
   
-  # behav 1
-  # ensure that there is one center cell
-  # if(sizes[1]%%2==0){sizes[1]=sizes[1]+1}
-  #  ss=sum(gausskern.nostd(sizes[1],sigmas[1],muadv=muadvs[1]))
-  #  while(ss<.999){
-  #    sizes[1]=sizes[1]+2
-  #    ss=sum(gausskern.nostd(sizes[1],sigmas[1],muadv=muadvs[1]))
-  #  }
-  K1 = gausskern.pg(sizes[1],sigmas[1],muadv=muadvs[1])
-  #  rm(ss)
-  #  K1=mask.K(K1)
+  if (kernel == 'normal'){
+    # behav 1
+    K1 = gausskern.pg(sizes[1], sigmas[1], muadv = muadvs[1])
+    
+    # behav 2
+    K2 = gausskern.pg(sizes[2], sigmas[2], muadv = muadvs[2])
+    
+  } else if (kernel == 'isotropic'){
+    
+    # behav 1
+    K1 = gausskern.isotrop(sizes[1], sigmas[1], muadv = muadvs[1], ratio.xy = 1)
+    
+    # behav 2
+    K2 = gausskern.isotrop(sizes[2], sigmas[2], muadv = muadvs[2], ratio.xy = 1)
+    
+  }
+
+  P <- matrix(c(pb[1], 1 - pb[1], 1 - pb[2], pb[2]), 2, 2, byrow = TRUE)
   
-  # behav 2
-  # if(sizes[2]%%2==0){sizes[2]=sizes[2]+1}
-  # ss=sum(gausskern.nostd(sizes[2],sigmas[2],muadv=muadvs[2]))
-  #  while(ss<.999){
-  #    sizes[2]=sizes[2]+2
-  #    ss=sum(gausskern.nostd(sizes[2],sigmas[2],muadv=muadvs[2]))
-  #  }
-  K2 = gausskern.pg(sizes[2],sigmas[2],muadv=muadvs[2])
-  #  rm(ss)
-  #  K2=mask.K(K2)
-  
-  P <- matrix(c(pb[1],1-pb[1],1-pb[2],pb[2]),2,2,byrow=TRUE)
-  #f. <- hmm.filter.2B.dealNA(g, L, K1,K2, P)
-  f. <- hmm.filter(g, L, K1,K2, P,maskL=maskL,bound.thr = bound.thr, minBounds = minBounds)
+  f. <- hmm.filter(g, L, K1, K2, P, maskL = maskL, bound.thr = bound.thr, minBounds = minBounds)
   nllf. <- -sum(log(f.$psi))
   return(nllf.)
 }
--- R/pos.log.lik.fun.r
+++ R/pos.log.lik.fun.r
@@ -1,38 +1,44 @@
-pos.log.lik.fun <- function(pars,g,L,maskL=TRUE,bound.thr = 0.1, minBounds = 10){
+#' Calculate positive log likelihood
+#' 
+#' \code{pos.log.lik.fun} calculates the positive log likelihood
+#' 
+#' @param pars kernel parameters. Usually output from \code{est_params}
+#' @param g see \code{hmm.filter}
+#' @param L see \code{hmm.filter}
+#' @param maskL see \code{hmm.filter}
+#' @param bound.thr see \code{hmm.filter}
+#' @param minBounds see \code{hmm.filter}
+#' @param kernel character either "normal" or "isotropic", depending on desired kernel characteristics
+#' @details For use in \code{GA::ga()} algorithm which maximizes, instead of minimizes, the cost/objective function
+#' @return a positive log likelihood
+#' @author Paul Gatti
+#' 
+
+pos.log.lik.fun <- function(pars, g, L, maskL = TRUE, bound.thr = 0.1, minBounds = 10, kernel = 'normal'){
+  
   # GA algorithm maximize instead of minimizing the cost/objective function
-  # pars=c(3,1,.7,.8)
+  
   sigmas=pars[1:2]
   sizes=rep(ceiling(sigmas[1]*4),2)
   pb=pars[3:4]
   muadvs=c(0,0)
   
-  # if(!is.na(pars[5])) muadvs[1]<-pars[5]
-  # if(!is.na(pars[6])) muadvs[2]<-pars[6]
-  
-  # behav 1
-  #  if(sizes[1]%%2==0){sizes[1]=sizes[1]+1}
-  #  ss=sum(gausskern.nostd(sizes[1],sigmas[1],muadv=muadvs[1]))
-  #  while(ss<.999){
-  #    sizes[1]=sizes[1]+2
-  #    ss=sum(gausskern.nostd(sizes[1],sigmas[1],muadv=muadvs[1]))
-  #  }
-  K1 = gausskern.pg(sizes[1],sigmas[1],muadv=muadvs[1])
-  # rm(ss)
-  #K1=mask.K(K1)
-  
-  # behav 2
-  #  if(sizes[2]%%2==0){sizes[2]=sizes[2]+1}
-  #  ss=sum(gausskern.nostd(sizes[2],sigmas[2],muadv=muadvs[2]))
-  #  while(ss<.999){
-  #    sizes[2]=sizes[2]+2
-  #    ss=sum(gausskern.nostd(sizes[2],sigmas[2],muadv=muadvs[2]))
-  #  }
-  K2 = gausskern.pg(sizes[2],sigmas[2],muadv=muadvs[2])
-  # rm(ss)
-  #  K2=mask.K(K2)
+  if (kernel == 'normal'){
+    # behav 1
+    K1 = gausskern.pg(sizes[1], sigmas[1], muadv = muadvs[1])
+    # behav 2
+    K2 = gausskern.pg(sizes[2], sigmas[2], muadv = muadvs[2])
+    
+  } else if (kernel == 'isotropic'){
+    
+    # behav 1
+    K1 = gausskern.isotrop(sizes[1], sigmas[1], muadv = muadvs[1], ratio.xy = 1)
+    # behav 2
+    K2 = gausskern.isotrop(sizes[2], sigmas[2], muadv = muadvs[2], ratio.xy = 1)
+    
+  }
   
   P <- matrix(c(pb[1],1-pb[1],1-pb[2],pb[2]),2,2,byrow=TRUE)
-  #f. <- hmm.filter.2B.dealNA(g, L, K1,K2, P)
   f. <- hmm.filter(g, L, K1,K2, P,maskL=maskL,bound.thr = bound.thr, minBounds = minBounds)
   pllf. <- sum(log(f.$psi)) 
   return(pllf.)

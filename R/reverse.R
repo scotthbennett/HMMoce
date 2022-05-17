10768b85b201beb1fae1d4fbabedb79e8b075060
Martini Arostegui <56367866+marosteg@users.noreply.github.com>
2022-05-17 07:21:07 -0400

    Update reverse.R

--- R/reverse.R
+++ R/reverse.R
@@ -1,4 +1,4 @@
 #Function for manipulating arrays and viewing matrix as image
-#Needed for code in hmm.posteriorviterbi.R
+#Needed for code in hmm.posteriorviterbi.R and hmm.filterExp.R
 
 reverse <- function(m) t(m)[,nrow(m):1] #Reverse lats for using image command

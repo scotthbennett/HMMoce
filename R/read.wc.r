debf6bc8443bbf5dbde8b79c0a8fd06ef949b1a7
Camrin Braun <camrin.braun@gmail.com>
2023-06-10 08:31:23 -0400

    Update read.wc.r
    deal with #81 where PDTs were eliminated when <3 depth levels were represented. now it is all-inclusive

--- R/read.wc.r
+++ R/read.wc.r
@@ -19,9 +19,10 @@
 #' @importFrom dplyr summarise
 #' @importFrom dplyr n
 #'   
-#' @return a list containing: the data read as a data.frame and a date vector of
-#'   unique dates in that data
+#' @return a the data read as a data.frame
+#' 
 #' @examples
+#' \dontrun{
 #' # example data in the package
 #' sstFile <- system.file("extdata", "141259-SST.csv", package = "HMMoce")
 #' ptt <- 141259
@@ -37,6 +38,7 @@
 #'
 #' # read and format the example data
 #' tag.sst <- read.wc(ptt, sstFile, type = 'sst', tag=tag, pop=pop)
+#' }
 #' @export
 
 read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=FALSE){
@@ -51,7 +53,7 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
     #data <- utils::read.table(paste(wd, ptt,'-PDTs.csv', sep=''), sep=',',header=T,blank.lines.skip=F, skip = 0)
     #data <- utils::read.csv(filename)# sep=',', header=T, blank.lines.skip=F, skip = 0)
     data <- utils::read.table(filename, sep=',', header=T, blank.lines.skip=F, skip = 0)
-    if (length(grep('Discont16', names(data))) == 0 & ncol(data) > 89) names(data)[90:94] <- c('Depth16','MinTemp16','MaxTemp16','X.Ox16','Discont16')
+    #if (length(grep('Discont16', names(data))) == 0 & ncol(data) > 89) names(data)[90:94] <- c('Depth16','MinTemp16','MaxTemp16','X.Ox16','Discont16')
     if (verbose) print(paste('If read.wc() fails for type=pdt, check the number of column headers in the PDTs.csv file.'))
     data <- extract.pdt(data)
     if (is.null(dateFormat)){
@@ -63,19 +65,20 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
     }
     data$Date <- dts
     d1 <- as.POSIXct('1900-01-02') - as.POSIXct('1900-01-01')
-    didx <- dts >= (tag + d1) & dts <= (pop - d1)
+    #didx <- dts >= (tag + d1) & dts <= (pop - d1)
+    didx <- dts >= tag & dts <= pop
     data <- data[didx,]; dts <- dts[didx]
     
     # check for days with not enough data for locfit
     data1 <- data
     data1$dts <- as.Date(dts)
     dt.cut <- data.frame(group_by(data1, dts) %>% summarise(n=n()))
-    dt.cut <- dt.cut[which(dt.cut[,2] < 3),1]
-    if (length(dt.cut) == 0){
-      
-    } else{
-      data <- data1[-which(data1$dts %in% dt.cut),]
-    }
+    #dt.cut <- dt.cut[which(dt.cut[,2] < 3),1]
+    #if (length(dt.cut) == 0){
+    #  
+    #} else{
+    #  data <- data1[-which(data1$dts %in% dt.cut),]
+    #}
     
     # get unique dates
     udates <- unique(as.Date(data$Date))
@@ -86,6 +89,7 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
     #print(paste('Data gaps are ', paste(gaps[gaps > 1], collapse=', '), ' days in PDT...'))
     gaps <- diff(c(as.Date(tag), udates, as.Date(pop)), units='days')
     if(verbose){
+      print(utils::head(data))
       print(paste(length(which(as.Date(seq(tag, pop, 'day')) %in% udates)), ' of ', length(seq(tag, pop, 'day')), ' deployment days have PDT data...', sep=''))
       print(paste('Data gaps are ', paste(gaps[gaps > 1], collapse=', '), ' days in PDT...'))
     }
@@ -103,18 +107,20 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
     }
     data$Date <- dts
     d1 <- as.POSIXct('1900-01-02') - as.POSIXct('1900-01-01')
-    didx <- dts >= (tag + d1) & dts <= (pop - d1)
+    #didx <- dts >= (tag + d1) & dts <= (pop - d1)
+    didx <- dts >= tag & dts <= pop
     data <- data[didx,]
     if (length(data[,1]) < 1){
       stop('Something wrong with reading and formatting of tags SST data. Check date format.')
     }
     dts <- as.POSIXct(data$Date, format = findDateFormat(data$Date))
     udates <- unique(as.Date(dts))
-    data <- data[,c(1:11)]
+    data <- data[,c('Date','Depth','Temperature')]
     
     # get data gaps
     gaps <- diff(c(as.Date(tag), udates, as.Date(pop)), units='days')
     if (verbose){
+      print(utils::head(data))
       print(paste(length(which(as.Date(seq(tag, pop, 'day')) %in% udates)), ' of ', length(seq(tag, pop, 'day')), ' deployment days have SST data...', sep=''))
       print(paste('Data gaps are ', paste(gaps[gaps > 1], collapse=', '), ' days...'))
     }
@@ -122,16 +128,22 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
   } else if(type == 'light'){
     # READ IN LIGHT DATA FROM WC FILES
     #data <- utils::read.table(paste(wd,'/', ptt, '-LightLoc.csv', sep=''), sep=',',header=T, blank.lines.skip=F,skip=2)
-    data <- utils::read.table(filename, sep=',',header=T, blank.lines.skip=F,skip=2)
-    if(!any(grep('depth', names(data), ignore.case=T))) data <- utils::read.table(filename, sep=',',header=T, blank.lines.skip=F,skip=1)
-    if(!any(grep('depth', names(data), ignore.case=T))) data <- utils::read.table(filename, sep=',',header=T, blank.lines.skip=F,skip=0)
+    data <- try(utils::read.table(filename, sep=',',header=T, blank.lines.skip=F, skip=2), TRUE)
+    
+    if (class(data) == 'try-error'){
+      data <- try(utils::read.table(filename, sep=',',header=T, blank.lines.skip=F, skip=0), TRUE)
+      if (class(data) == 'try-error') stop('Tried reading light data with skip=2 (old WC format) and skip=0 (new WC format) but both failed. Check source light data file and try again.')
+    }
+    
+    if(!any(grep('depth', names(data), ignore.case=T))) data <- utils::read.table(filename, sep=',',header=T, blank.lines.skip = F, skip = 1)
+    if(!any(grep('depth', names(data), ignore.case=T))) data <- utils::read.table(filename, sep=',',header=T, blank.lines.skip = F, skip = 0)
     data <- data[which(!is.na(data[,1])),]
     
-    #dts <- as.POSIXct(data$Day, format = findDateFormat(data$Day), tz = 'UTC')
-    dts <- as.POSIXct(data$Day, format = '%d-%b-%y', tz = 'UTC')
+    #dts <- as.POSIXct(data$Day, format = findDateFormat(data$Day))
+    dts <- lubridate::parse_date_time(data$Day, orders=c('dby', 'dbY'), tz='UTC')
     
     if(as.Date(dts[1]) > as.Date(Sys.Date()) | as.Date(dts[1]) < '1990-01-01'){
-      stop('Error: dates not parsed correctly.')    
+      stop('Error: dates are in the future or before 1990 and thus likely did not parse correctly.')    
     }
     
     d1 <- as.POSIXct('1900-01-02') - as.POSIXct('1900-01-01')
@@ -143,13 +155,15 @@ read.wc <- function(filename, tag, pop, type = 'sst', dateFormat=NULL, verbose=F
     # get data gaps
     gaps <- diff(c(as.Date(tag), udates, as.Date(pop)), units='days')
     if (verbose){
+      print(utils::head(data))
       print(paste(length(which(as.Date(seq(tag, pop, 'day')) %in% udates)), ' of ', length(seq(tag, pop, 'day')), ' deployment days have light data...', sep=''))
       print(paste('Data gaps are ', paste(gaps[gaps > 1], collapse=', '), ' days...'))
     }
     
   }
   
-  return(list(data = data, udates = udates))
+  #return(list(data = data, udates = udates))
+  return(data)
   
 }
 
